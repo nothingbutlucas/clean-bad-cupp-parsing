7d31f932f43f688553b90d42e29d64a6f9dab8fc
Lucas <69118979+nothingbutlucas@users.noreply.github.com>
2022-05-19 17:35:53 -0300

    Fix another 2 cupp errors
    When cupp puts a #, does it with a quotation mark '#' and that ruins the wordlist. Also that at the beginning of each generated wordlist the first 2 passwords are '#''#''#'.

--- clean_cupp.py
+++ clean_cupp.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-
 import sys
 from pwn import log, time
 
@@ -10,7 +9,7 @@ try:
     file_for_write = sys.argv[2]
 except:
     log.info("Give 2 arguments!")
-    log.info("Example: python3 clean_spanish.py words.spanish words.spanish.fix")
+    log.info("Example: python3 clean_cupp.py words.spanish words.spanish.fix")
     exit()
 with open(file_for_read, 'r') as read_file, open(file_for_write, 'w') as write_file:
     p = log.progress(f"Reading {file_for_read}, writing in {file_for_write}")
@@ -23,6 +22,10 @@ with open(file_for_read, 'r') as read_file, open(file_for_write, 'w') as write_f
             word = word.replace("i`", "í")
             word = word.replace("o`", "ó")
             word = word.replace("u`", "ú")
-
-        write_file.write(word)
+        if word != "###":
+            p.status(f"{word} is not ###")
+            if "'#'" in word:
+                p.status(f"Find an '#' in {word}")
+                word = word.replace("'#'","#")
+            write_file.write(word)
     p.success("Finish!")
